837619b65db2e684a05477aa6f10dec0995f092c
Shishir Shekhar <71517975+ShishirShekhar@users.noreply.github.com>
2021-09-24 23:15:17 +0530

    scope page

--- pages/scope.py
+++ pages/scope.py
@@ -1,5 +1,16 @@
+"""This page is for future scopes of our model"""
+
+# Import necessary modules
 import streamlit as st
 
 
 def app():
+    """This function runs the model info page"""
+    # Add title to page
     st.title("Welcome to the project scope page")
+
+    # Add image
+    st.image("./images/iris_classification_model.jpg")
+
+    # Add model description
+    st.write("project scope")

aeea94d4098643194ccf2522dfe3476738a36ceb
Shishir Shekhar <71517975+ShishirShekhar@users.noreply.github.com>
2021-09-24 23:11:07 +0530

    about

--- pages/about.py
+++ pages/about.py
@@ -1,5 +1,33 @@
+"""This is about page"""
+
+# import necessary modules
 import streamlit as st
 
 
 def app():
+    """This function runs the about page"""
+    # Add balloons animation when page opens
+    st.balloons()
+
+    # Add title
     st.title("Welcome to the about me page")
+
+    # Add an image
+    st.image("./images/iris_classification_model.jpg")
+    
+    # Add Contact Details
+    st.header('Contact Us')
+
+    # Add email
+    st.markdown('''### Name:
+    Shishir Shekhar''')
+
+    # Add name
+    st.markdown('''### Email:
+    sspdav02@gmail.com''')
+
+    # Add github
+    st.markdown('''### GitHub: [ShishirShekhar](https://github.com/ShishirShekhar/)''')
+
+    # Add linkedin
+    st.markdown('''### Linkedin: [ShishirShekhar](https://www.linkedin.com/in/shishir-shekhar/)''')
